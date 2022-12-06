7fdce56ef09a7913ac0feb6ecab9c06e59cc9a54
Musa Alfian Maulana <musaganteng071@gmail.com>
2022-12-06 11:05:44 +0700

    fixing use

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Hash;
 use Session;
@@ -9,54 +10,61 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('auth.login2');
     }
-    public function customLogin(Request $request){
+    public function customLogin(Request $request)
+    {
         $request->validate([
-            'email'=>'required',
-            'password'=>'required',
+            'email' => 'required',
+            'password' => 'required',
         ]);
-        $credentials=$request->only('email','password');
-        if(Auth::attempt($credentials)){
-            if(Auth::user()->role==1){
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            if (Auth::user()->role == 1) {
                 $email = Auth::user()->email;
-                return redirect()->route('beranda', compact('email'))->with('success','Signed in');
-            }else{
+                return redirect()->route('beranda', compact('email'))->with('success', 'Signed in');
+            } else {
                 $email = Auth::user()->email;
-                return redirect()->route('admin.index')->with('success','Signed in');  
+                return redirect()->route('admin.index')->with('success', 'Signed in');
             }
         }
         return redirect('login')->with('Login details are not valid');
     }
-    public function registration(){
+    public function registration()
+    {
         return view('auth.registration');
     }
-    public function customRegistration(Request $request){
+    public function customRegistration(Request $request)
+    {
         $request->validate([
-            'email'=>'required',
-            'name'=>'required',
-            'password'=>'required|min:6'
+            'email' => 'required',
+            'name' => 'required',
+            'password' => 'required|min:6'
         ]);
         $data = $request->all();
         $check = $this->create($data);
 
         return redirect('dashboard')->withSuccess('You have signed-in');
     }
-    public function create(array $data){
+    public function create(array $data)
+    {
         return User::create([
-            'name'=>$data['name'],
-            'email'=>$data['email'],
-            'password'=>Hash::make($data['password'])
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
         ]);
     }
-    public function dashboard(){
-        if(Auth::check()){
+    public function dashboard()
+    {
+        if (Auth::check()) {
             return view('dashboard');
         }
         return redirect('login')->withSuccess('You are not allowed to access');
     }
-    public function signOut(){
+    public function signOut()
+    {
         Session::flush();
         Auth::logout();
         return Redirect('login');
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\LogController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -46,52 +47,52 @@ use Illuminate\Http\Request;
 //Saat akses ke route yang tidak terdefinisi
 Route::fallback(function () {
     return view('404');
-}); 
+});
 
 //Autentikasi
-Route::get('/', function(Request $request){
-    return view('user.login'); 
+Route::get('/', function (Request $request) {
+    return view('user.login');
 });
 Route::get('/login', function (Request $request) {
     return view('user.login');
 })->name('login');
 Route::get('signout', [AuthController::class, 'signOut'])->name('signout');
-Route::post('custom-login', [AuthController::class, 'customLogin'])->name('login.custom'); 
+Route::post('custom-login', [AuthController::class, 'customLogin'])->name('login.custom');
 
 
-Route::middleware(['checkAuth'])->group(function(){
+Route::middleware(['checkAuth'])->group(function () {
 
     //Section Normal User
-    Route::middleware(['checkUser'])->group(function(){
+    Route::middleware(['checkUser'])->group(function () {
         Route::get('/beranda', function (Request $request) {
-            return view('user.beranda',['email'=>Auth::user()->email]);
+            return view('user.beranda', ['email' => Auth::user()->email]);
         })->name('beranda');
         Route::get('/informasi', function (Request $request) {
-            return view('user.informasi',['email'=>Auth::user()->email]);
+            return view('user.informasi', ['email' => Auth::user()->email]);
         });
-        Route::get('/informasi',[InformationController::class,'userInformation'])->name('user.information');
-        Route::get('/upload-kegiatan',[ReportController::class,'uploadKegiatan'])->name('upload-kegiatan');
-        Route::post('/store-kegiatan',[ReportController::class,'store'])->name('storeKegiatan');
-        Route::get('/laporan-kegiatan',[ReportController::class,'indexDatatable'])->name('indexDatatable');            
+        Route::get('/informasi', [InformationController::class, 'userInformation'])->name('user.information');
+        Route::get('/upload-kegiatan', [ReportController::class, 'uploadKegiatan'])->name('upload-kegiatan');
+        Route::post('/store-kegiatan', [ReportController::class, 'store'])->name('storeKegiatan');
+        Route::get('/laporan-kegiatan', [ReportController::class, 'indexDatatable'])->name('indexDatatable');
     });
 
     //Section Admin
-    Route::middleware(['checkAdmin'])->group(function(){
-        Route::get('reports/{id}/edit',[ReportController::class,'edit']);
-        Route::get('/download/{id}',[ReportController::class,'download'])->name('report.download');
+    Route::middleware(['checkAdmin'])->group(function () {
+        Route::get('reports/{id}/edit', [ReportController::class, 'edit']);
+        Route::get('/download/{id}', [ReportController::class, 'download'])->name('report.download');
         // Route::get('/index-admin', function(Request $request){
         //     return view('admin.index');
         // })->name('admin.index');
         // Route::get('/edit/{id}',[InformationController::class,'edit'])->name('information.edit'); susah
-        Route::get('/delete/{id}',[InformationController::class,'delete'])->name('information.delete');
-        Route::post('/store-kategori',[KategoriController::class,'store'])->name('kategori.store');
-        Route::post('/store-information',[InformationController::class,'store'])->name('information.store');
-        Route::get('/index-admin',[ReportController::class,'indexAdmin'])->name('admin.index');
-        Route::get('/log',[LogController::class,'log'])->name('admin.log');
-        Route::get('/laporan-kegiatan-json',[ReportController::class,'getJson'])->name('getJson');
-        Route::get('/prodi',[ReportController::class,'prodi'])->name('admin.prodi');
-            // return view('admin.prodi');
-        Route::get('/admin-informasi',[InformationController::class,'adminInformation'])->name('admin.informasi');
+        Route::get('/delete/{id}', [InformationController::class, 'delete'])->name('information.delete');
+        Route::post('/store-kategori', [KategoriController::class, 'store'])->name('kategori.store');
+        Route::post('/store-information', [InformationController::class, 'store'])->name('information.store');
+        Route::get('/index-admin', [ReportController::class, 'indexAdmin'])->name('admin.index');
+        Route::get('/log', [LogController::class, 'log'])->name('admin.log');
+        Route::get('/laporan-kegiatan-json', [ReportController::class, 'getJson'])->name('getJson');
+        Route::get('/prodi', [ReportController::class, 'prodi'])->name('admin.prodi');
+        // return view('admin.prodi');
+        Route::get('/admin-informasi', [InformationController::class, 'adminInformation'])->name('admin.informasi');
         // Route::get('/admin-informasi',function(Request $request){
         //     return view('admin.informasi');
         // })->name('admin.informasi');
